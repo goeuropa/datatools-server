750072296b759a3e5e9a826bfcb35f740fe0b9ed
Grzegorz Patynek <gpatynek@gmail.com>
2023-06-01 18:23:21 +0200

    baza

--- src/main/java/com/conveyal/datatools/manager/controllers/api/StatusController.java
+++ src/main/java/com/conveyal/datatools/manager/controllers/api/StatusController.java
@@ -124,12 +124,12 @@ public class StatusController {
           PdfGenerator pdf = new PdfGenerator();
           //PrzystanekD p = pdf.generujKombus(number);
 
-          String databaseUrl = "jdbc:postgresql://localhost/catalogue";
+          String databaseUrl = "jdbc:postgresql://postgres/dmtest";//jdbc:postgresql://localhost/catalogue
           PrzystanekD p = new PrzystanekD();
           GraphQLController.initialize(GTFS.createDataSource(databaseUrl, null, null), apiPrefix);
           System.out.println("GraphQL query null");
           GraphQLController.getGraphQL();
-          
+
           pdf.generujPrzystanek(p, "output2.pdf");
 
 

216b94dcd923d1f211c436079f16406036fbabca
Grzegorz Patynek <gpatynek@gmail.com>
2023-07-19 01:51:38 +0200

    regieters cors

--- src/main/java/com/conveyal/datatools/manager/controllers/api/AppInfoController.java
+++ src/main/java/com/conveyal/datatools/manager/controllers/api/AppInfoController.java
@@ -26,13 +26,6 @@ public class AppInfoController {
         // TODO: convert into a POJO if more stuff is needed here. Although there would need to be an accommodation for
         //  the config field, which has a structure that is dependent on whatever is included in server.yml.
 
-        System.out.println("l");
-        System.out.println(JsonUtil.objectMapper.createObjectNode()
-            .put("repoUrl", repoUrl)
-            .put("commit", commit)
-            // Add config variables. NOTE: this uses server.yml, which is not intended to have any secure information.
-            .putPOJO("config", DataManager.getPublicConfig()));
-            //TODO: println debug
         return JsonUtil.objectMapper.createObjectNode()
             .put("repoUrl", repoUrl)
             .put("commit", commit)
@@ -41,27 +34,7 @@ public class AppInfoController {
     }
 
     public static void register (String apiPrefix) {
-      // Enable CORS
-        options("/*", (request, response) -> {
-            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
-            if (accessControlRequestHeaders != null) {
-                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
-            }
 
-            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
-            if (accessControlRequestMethod != null) {
-                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
-            }
-
-            return "OK";
-        });
-
-        before((request, response) -> {
-            response.header("Access-Control-Allow-Origin", "*");
-            response.header("Access-Control-Request-Method", "GET, POST, PUT, DELETE, OPTIONS");
-            response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-            // You can add other headers if needed
-        });
         get(apiPrefix + "public/appinfo", AppInfoController::getInfo, JsonUtil.objectMapper::writeValueAsString);
     }
 }
